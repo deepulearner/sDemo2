35c051b97b6c908d57c83b02e1bc7a95f8628463
deepulearner <deepulearner@gmail.com>
2022-03-02 23:18:23 +0530

    Sauce Demo Login Study

---
+++ Test Suites/ts_2.ts
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>ts_2</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>b6f00424-f9ec-41de-8b57-5d4cfcc91720</testSuiteGuid>
+   <testCaseLink>
+      <guid>6f705ca2-146b-4294-8d90-77cedd3e8f0b</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/tc_2</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
